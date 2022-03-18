5563da0abc48392375b8c4f9eda748292d9620e9
SonyKar <sa.caraganciu@gmail.com>
2022-03-18 12:05:05 +0200

    init commit

---
+++ MainPage.xaml.h
@@ -0,0 +1,21 @@
+﻿//
+// MainPage.xaml.h
+// Declaration of the MainPage class.
+//
+
+#pragma once
+
+#include "MainPage.g.h"
+
+namespace Assembler_for_Virtual_Processor
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public ref class MainPage sealed
+	{
+	public:
+		MainPage();
+
+	};
+}
